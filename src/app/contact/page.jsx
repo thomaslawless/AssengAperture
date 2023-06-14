2ec59233f13e8fc902d40acc496346f25b59ab3c
Tom Lawless <lawlessptom@gmail.com>
2023-06-14 12:03:56 -0400

    updating contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 import Button from "@/components/Button/Button";
 
 export const metadata = {
-  title: "Lama Dev Contact Information",
+  title: "Contact",
   description: "This is Contact Page",
 };
 
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <h1 className={styles.title}>Keep in Touch</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image
