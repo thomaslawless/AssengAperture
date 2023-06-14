8ffcfad0c3d74b3b6ece7146d82b844de3a544dc
Tom Lawless <lawlessptom@gmail.com>
2023-06-14 12:00:29 -0400

    edit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import React from "react";
-import styles from "./navbar.module.css";
+import styles from "./navbar.module.css"
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import { signOut, useSession } from "next-auth/react";
 
