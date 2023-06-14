39426f8e9b472f86d6f21be4b78f80a84e15c99f
Tom Lawless <lawlessptom@gmail.com>
2023-06-14 10:59:59 -0400

    auth

--- src/models/User.js
+++ src/models/User.js
@@ -1,23 +1,25 @@
 import mongoose from "mongoose";
 
-const {Schema} = mongoose
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        unique: true,
-        required: true,
+      type: String,
+      unique: true,
+      required: true,
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
+      type: String,
+      unique: true,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-}, {timestamps: true}
+  },
+  { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema)
+export default mongoose.model("User", userSchema);
